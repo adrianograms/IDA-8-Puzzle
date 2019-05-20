4eb5836221ec3dbf2537a52780eb3464f3e41940
vangolax <mateus.edival@gmail.com>
2019-05-19 22:42:31 -0300

    Limpeza

--- main.cpp
+++ main.cpp
@@ -1,11 +1,4 @@
-
-
-#include <iostream> 
 #include <bits/stdc++.h>
-#include <string.h>
-#include <stack> 
-#include <vector>
-#include <map>
 
 using namespace std; 
 
@@ -69,7 +62,6 @@ struct Node {
     e setar seus valores de hx e gx, alem do vetor ao qual eles fazem parte
     */
     void processamentofilhos(int x_src,int y_scr,int x_dest,int y_dest) {
-		
 		if(pai!= NULL && pai->matriz[x_src][y_scr] == 0)
 			return;
 
@@ -83,17 +75,15 @@ struct Node {
         memcpy(matriz_aux, this->matriz, sizeof(char)*9);
         matriz_aux[x_dest][y_dest] = matriz[x_src][y_scr];
         matriz_aux[x_src][y_scr] = 0;
-        //cout << hashcode(matriz_aux) << endl;
+        
         if(matrizes_existentes.count(hashcode(matriz_aux)))
 			return;
         Node *n;
         if(valorAtual > proximoValor) {
             n = new Node(this,matriz_aux,gx+1,hx-1);
-            //filhos.push(n);
         }
         else {
             n = new Node(this,matriz_aux,gx+1,hx+1);
-            //filhos.push(n);
         }
 
         if(n->valorfx == this->valorfx)
@@ -102,11 +92,6 @@ struct Node {
             limitesInvalidos_global.push_back(n);
             
         matrizes_existentes[hashcode(n->matriz)] = true;
-        
-
-
-
-
 }
 
     /*
@@ -121,30 +106,6 @@ struct Node {
 
                     if(x+1 != 3) {
                         processamentofilhos(x+1,y,x,y);
-                        /*
-                        int aux = x+1;
-
-                        int valorAtual = abs(aux - ((int)matriz[x+1][y]/3)) 
-                        + abs(y - ((int)matriz[x+1][y]%3));
-
-                        int proximoValor = abs(x - ((int)matriz[x+1][y]/3)) 
-                        + abs(y - ((int)matriz[x+1][y]%3));
-
-                        char matriz_aux[3][3];
-                        memcpy(matriz_aux, this->matriz, sizeof(char)*9);
-                        matriz_aux[x][y] = matriz[x+1][y];
-                        matriz_aux[x+1][y] = 0;
-
-                        if(valorAtual > proximoValor) {
-                            Node n = Node(this,matriz_aux,gx+1,hx-1);
-                            filhos.push(n);
-                        }
-                        else {
-                            Node n = Node(this,matriz_aux,gx+1,hx+1);
-                            filhos.push(n);
-                        }
-                        */
-                        
                     }
                     if(x-1 != -1) {
                         processamentofilhos(x-1,y,x,y);
@@ -160,6 +121,7 @@ struct Node {
             }
         }
     }
+    
     string static hashcode(char matriz[3][3]) {
 		string hash;
 		for(int i =0; i< 3;i++) {
@@ -195,25 +157,12 @@ void run() {
         }
         cout << endl;
     }
-    cout <<matrizes_existentes.size() << endl;
-
+    cout << no->gx << endl;
+    cout << matrizes_existentes.size() << endl;
+	matrizes_existentes.clear();
 }
   
 int main() 
 { 
-    char matriz[3][3];
-    for(int x=0;x<3;x++)
-    {
-        for(int y=0;y<3;y++)
-        {
-            matriz[x][y]='a';
-        }
-    }
-    Node n = Node(matriz, 0,0);
-    cout << n.matriz[2][1] << endl;
-    vector<Node*> teste;
-    Node a = Node();
-    teste.push_back(&a);
-    a.gx = 2;
     run();
 } 
